12517fda4c6583feed8324b05ad2602db3252622
Vishakha2301 <vishakha.disawal@gmail.com>
2026-03-02 17:56:01 +0530

    initial commit

---
+++ solar-erp-app/src/main/java/com/solarerp/app/SolarErpAppApplication.java
@@ -0,0 +1,13 @@
+package com.solarerp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarErpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarErpAppApplication.class, args);
+	}
+
+}
---
+++ solar-erp-app/src/test/java/com/solarerp/app/SolarErpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solarerp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarErpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
