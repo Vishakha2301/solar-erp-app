9927f94b70bd814b8bc059654d64bfbfacc7a1fd
Vishakha Ankur Potdar <62894099+Vishakha2301@users.noreply.github.com>
2026-04-06 14:42:29 +0530

    Create CustomerRepository.java

---
+++ solar-erp-common/src/main/java/com/solarerp/customer/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.solarerp.customer.repository;
+
+import com.solarerp.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    List<Customer> findAllByActiveTrueOrderByNameAsc();
+
+    List<Customer> findByNameContainingIgnoreCaseAndActiveTrue(String name);
+}
