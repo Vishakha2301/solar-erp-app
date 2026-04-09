a48a1fcaadbb4a5a1aeecaefccaf3634d18697f8
Vishakha Ankur Potdar <62894099+Vishakha2301@users.noreply.github.com>
2026-04-09 12:06:42 +0530

    Create DocumentGenerationException.java

---
+++ solar-erp-common/src/main/java/com/solarerp/exception/DocumentGenerationException.java
@@ -0,0 +1,12 @@
+package com.solarerp.exception;
+
+public class DocumentGenerationException extends RuntimeException {
+
+    public DocumentGenerationException(String message) {
+        super(message);
+    }
+
+    public DocumentGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
